3cc63570a0fd7db0ab66e9dc1393a5897012f1ea
Vitalii Naumov <vitalii.nau@gmail.com>
2024-04-05 12:23:21 +0300

    finished main file

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ def find_top_words(file_path, n=10):
     top_words = word_counts.most_common(n)
 
     return top_words
+
+def write_to_file(top_words, output_file):
+    #Запис результату у файл
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for word, count in top_words:
+            file.write(f"{word}-{count}\n")
+
+#Параметри скрипту
+input_file = "input.txt"
+output_file = "output.txt"
+top_n = 10
+
+#Знаходження та запис 10 найпопулярніших слів у файл
+top_words = find_top_words(input_file, top_n)
+write_to_file(top_words, output_file)
